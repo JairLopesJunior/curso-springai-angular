b2a821432ba59a6cf9353129d807542adae33b99
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2025-08-26 10:26:40 -0300

    Spring AI + Angular: Log Advisor e Tokens

--- api-ai/src/main/java/com/jair/api_ai/services/MemoryChatService.java
+++ api-ai/src/main/java/com/jair/api_ai/services/MemoryChatService.java
@@ -3,7 +3,9 @@ package com.jair.api_ai.services;
 import com.jair.api_ai.chat.ChatMessage;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,17 +13,22 @@ public class MemoryChatService {
 
     private final ChatClient chatClient;
 
-    public MemoryChatService(ChatClient.Builder builder,
-                             ChatMemory chatMemory) {
+    public MemoryChatService(ChatClient.Builder builder) {
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .maxMessages(10)
+                .build();
+
         this.chatClient = builder
                 .defaultAdvisors(
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        new SimpleLoggerAdvisor()
                 )
                 .build();
     }
 
     public String memoryChat(String message) {
         return this.chatClient.prompt()
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "123456"))
                 .user(message)
                 .call()
                 .content();
